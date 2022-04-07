2a9d8238077d267e209e83a14e9ff18f049a6dfa
Seyun Park <seyun373@gmail.com>
2022-04-07 21:13:08 +0900

    세윤

---
+++ semiTeamProject/src/main/java/com/campus/myapp/vo/CodyVO.java
@@ -0,0 +1,48 @@
+package com.campus.myapp.vo;
+
+public class CodyVO {
+	private String cname;
+	private int temp;
+	private String weather;
+	private String sex;
+	private String style;
+	private String info;
+	
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public int getTemp() {
+		return temp;
+	}
+	public void setTemp(int temp) {
+		this.temp = temp;
+	}
+	public String getWeather() {
+		return weather;
+	}
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+}
