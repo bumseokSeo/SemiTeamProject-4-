89dd1fd7710342b8904b192fcc1f2425775d9ed2
jjjjjeeee <monami6901@gmail.com>
2022-04-13 17:53:40 +0900

    이정은 review 컨트롤러 수정

--- semiTeamProject/src/main/java/com/campus/myapp/controller/MapController.java
+++ semiTeamProject/src/main/java/com/campus/myapp/controller/MapController.java
@@ -7,13 +7,14 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.servlet.ModelAndView;
 
 import com.campus.myapp.service.MapService;
+import com.campus.myapp.service.ReviewService;
 import com.campus.myapp.vo.ReviewVO;
 
 @RestController
@@ -21,7 +22,8 @@ import com.campus.myapp.vo.ReviewVO;
 public class MapController {
 	@Inject
 	MapService service;
-	@GetMapping("main_map")
+	
+	@PostMapping("main_map")
 	public ModelAndView Main_map() {
 		//DB에서 음식메뉴 가져오기 ==> 서비스 
 		ModelAndView mav = new ModelAndView();
---
+++ semiTeamProject/src/main/java/com/campus/myapp/vo/StoreVO.java
@@ -0,0 +1,40 @@
+package com.campus.myapp.vo;
+
+public class StoreVO {
+    private String placename;
+    private String phone;
+    private String roadaddressname;
+    private String addressname;
+    private String placeid;
+
+    public String getPlacename() {
+        return placename;
+    }
+    public void setPlacename(String placename) {
+        this.placename = placename;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getRoadaddressname() {
+        return roadaddressname;
+    }
+    public void setRoadaddressname(String roadaddressname) {
+        this.roadaddressname = roadaddressname;
+    }
+    public String getPlaceid() {
+        return placeid;
+    }
+    public void setPlaceid(String placeid) {
+        this.placeid = placeid;
+    }
+    public String getAddressname() {
+        return addressname;
+    }
+    public void setAddressname(String addressname) {
+        this.addressname = addressname;
+    }
+}
