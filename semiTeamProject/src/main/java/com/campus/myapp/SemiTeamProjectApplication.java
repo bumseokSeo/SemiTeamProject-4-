d4f871fb1e5a6cea6829d8fc01993212fe1eca94
jjjjjeeee <monami6901@gmail.com>
2022-04-14 16:35:43 +0900

    이정은 리뷰페이지 수정, 파일올리기 db연동

--- semiTeamProject/src/main/java/com/campus/myapp/SemiTeamProjectApplication.java
+++ semiTeamProject/src/main/java/com/campus/myapp/SemiTeamProjectApplication.java
@@ -2,8 +2,10 @@ package com.campus.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={MultipartAutoConfiguration.class})
+
 public class SemiTeamProjectApplication {
 
 	public static void main(String[] args) {
